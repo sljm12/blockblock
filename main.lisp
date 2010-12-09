9102e6ab33720227ffd233ec0794aa42cc2c554f
unknown <Administrator@.(none)>
2010-12-09 17:31:52 +0800

    Added a higher order function map-draw-grid for looping through the grid
    Modified draw-grid to make use of this higher order function.
    
    Added draw-grid-at for drawing a grid at a particular place.

--- main.lisp
+++ main.lisp
@@ -1,5 +1,5 @@
-(setf *red-image* "g:/lisp/lispgame/red.bmp")
-(setf *blue-image* "g:/lisp/lispgame/blue.bmp")
+(setf *red-image* "c:/lisp/lispgame/red.bmp")
+(setf *blue-image* "c:/lisp/lispgame/blue.bmp")
 
 
 (defun run ()
@@ -13,7 +13,7 @@
 		       (when (SDL:KEY= KEY :SDL-KEY-ESCAPE) (SDL:PUSH-QUIT-EVENT)))
       (:idle ()
 	     ;(sdl:draw-surface (sdl:load-image *red-image*))
-	     ;(draw-grid *grid* 20 *grid-pic*)
+	     (draw-grid *grid* 20 *grid-pic*)
 	     (sdl:update-display))
       ;(:video-expose-event () (sdl:update-display))
       )
@@ -27,16 +27,40 @@
   
 )
 
+;(defun draw-grid (grid gridsize pic-store)
+;   (loop for y from 0 to 3 do
+;       (loop for x from 0 to 5 do
+;	    (cond ((not (= (get_gridnum x y grid) 0))
+;		   (sdl:draw-surface-at-* 
+;		    (get-image (get_gridnum x y *grid*) pic-store) 
+;		    (* x 20) (* y 20))))
+;	    )))
+
 (defun draw-grid (grid gridsize pic-store)
-  (loop for y from 0 to 3 do
-       (loop for x from 0 to 5 do
-	    (cond ((not (= (get_gridnum x y grid) 0))
-		   (sdl:draw-surface-at-* 
-		    (get-image (get_gridnum x y *grid*) pic-store) 
-		    (* x 20) (* y 20))))
-	    )
-       )	    
+  (map-draw-grid 3 3 #'draw-grid-at (list grid pic-store))
   )
+
+(defun draw-grid-at (x y grid pic-store)
+  (cond ((not (= (get_gridnum x y grid) 0))
+		 (sdl:draw-surface-at-* 
+		  (get-image (get_gridnum x y *grid*) pic-store) 
+		  (* x 20) (* y 20)))))
+
+
+
+;Higher order function for looping through the map grid, grid starts from 0
+;Max-x is the max size of the grid
+;Max-y is the max size of the grid in y
+;fn to execute minimium needs to have 2 parameters with the first being x and the second y
+;fn-parameters additional parameters to pass to fn
+(defun map-draw-grid (max-x max-y fn fn-parameters)
+	(loop for y from 0 to max-y do
+		 (loop for x from 0 to max-x do
+			  (apply fn (append (list x y) fn-parameters))
+			  )
+		 ))
+
+
 
 (defun load-grid-pics()
   (list (cons 1 (cons (sdl:load-image *red-image*) nil))
