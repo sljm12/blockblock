c6c32a79e1509c77873ebb41a383e73a3c4cd791
unknown <Administrator@.(none)>
2010-12-15 18:34:05 +0800

    Added
    draw-user-blocks
    -Draw the block that the user will be controlling
    
    draw-block
    -Help draw a color-block structure

--- main.lisp
+++ main.lisp
@@ -3,38 +3,45 @@
 
 
 (defun run ()
+  (let ((ub (make-user-block :b1 (make-color-block :x 1 :y 1 :color 1)
+			     :b2 (make-color-block :x 1 :y 1 :color 1)))
+	;(*grid-pic* (load-grid-pics))
+	)
+	     
   (sdl:with-init ()
     (sdl:window 320 240)
     (sdl:update-display)
     (setf *grid-pic* (load-grid-pics))
+    
     (sdl:with-events()
       (:quit-event () t)
       (:key-down-event (:key key)
 		       (when (SDL:KEY= KEY :SDL-KEY-ESCAPE) (SDL:PUSH-QUIT-EVENT)))
       (:idle ()
-	     ;(sdl:draw-surface (sdl:load-image *red-image*))
 	     (draw-grid *grid* 20 *grid-pic*)
+	     (draw-user-blocks ub *grid-pic*)
 	     (sdl:update-display))
-      ;(:video-expose-event () (sdl:update-display))
       )
-    )
+    ))
   )
 
 (defstruct color-block x y color)
 (defstruct user-block b1 b2)
 
-(defun draw-user-blocks ()
-  
-)
+(defun draw-user-blocks (user-block pic-store)
+  (let ((b1 (user-block-b1 user-block))
+	(b2 (user-block-b2 user-block)))
+    (if (not (eq b1 nil))
+	(draw-block b1 pic-store))
+    (if (not (eq b2 nil))
+	(draw-block b2 pic-store))
+    ))
 
-;(defun draw-grid (grid gridsize pic-store)
-;   (loop for y from 0 to 3 do
-;       (loop for x from 0 to 5 do
-;	    (cond ((not (= (get_gridnum x y grid) 0))
-;		   (sdl:draw-surface-at-* 
-;		    (get-image (get_gridnum x y *grid*) pic-store) 
-;		    (* x 20) (* y 20))))
-;	    )))
+(defun draw-block (c-block pic-store)
+  (sdl:draw-surface-at-* 
+   (get-image (color-block-color c-block)  pic-store) 
+   (color-block-x c-block) 
+   (color-block-y c-block)))
 
 (defun draw-grid (grid gridsize pic-store)
   (map-draw-grid 3 3 #'draw-grid-at (list grid pic-store 100 100))
